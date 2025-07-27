6ef59f2ddb4304c7494f714bf5b4588945631b9d
Luciano Zurlo <luciano@aiMac.local>
2025-07-27 17:27:34 -0300

    update

--- js/projects.js
+++ js/projects.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const splide = new Splide(sliderEl, {
          type: 'loop',
          perPage: 5,
+         perMove: 1,
          focus: 'center',
          drag: true,
          snap: true,
@@ -13,21 +14,27 @@ document.addEventListener('DOMContentLoaded', () => {
          updateOnMove: true,
          speed: 600,
          start: startIndex,
+
          breakpoints: {
             1279: {
                perPage: 3,
+               perMove: 1,
                focus: 'center',
                speed: 500,
+               flickMaxPages: 1,
+               flickPower: 450,
             },
             767: {
                perPage: 1,
+               perMove: 1,
                focus: 'center',
                speed: 400,
+               flickMaxPages: 1,
+               flickPower: 350,
             },
          },
       });
 
-      // Click en cualquier slide lo centra
       splide.on('click', (slide) => splide.go(slide.index));
 
       splide.mount();
